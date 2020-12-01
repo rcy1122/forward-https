0467b153bc5cb13a8c1d5b3e2b7a85f5468f9966
ran.chenyang <ran.chenyang@cloudminds.com>
2020-12-01 12:58:34 +0800

    add test

--- forward.go
+++ forward.go
@@ -216,7 +216,7 @@ func (fa *Forward) forwardRequest(req *http.Request) (*http.Request, error) {
 
 func (fa *Forward) queryAddressPort(req *http.Request) (string, error) {
 	host := req.URL.Host
-	newRequest, err := http.NewRequest(http.MethodGet, "https://cub.baas.com:443/health", nil)
+	newRequest, err := http.NewRequest(http.MethodGet, "https://cub.service.consul:443/health", nil)
 	if err != nil {
 		return "", fmt.Errorf("call cub %w", err)
 	}
